f10b192b9efce1997154199a3e34a594e7bb5c4e
kimhaechang <81299056+kimhaechang1@users.noreply.github.com>
2023-12-07 16:20:17 +0900

    Title: 1차원 젠가, Time: 79ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 231207/1차원 젠가/jenga-1d.java
@@ -0,0 +1,52 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int arrlen;
+    static int templen;
+    static int [] arr;
+    static int [] temp;
+    static int n;
+    static StringTokenizer stk;
+    public static void main(String[] args) throws Exception{
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(bf.readLine());
+        arrlen = n;
+        arr = new int[n];
+        templen = 0;
+        temp = new int[n];
+        for(int i = n-1;i>-1;i--){
+            arr[i] = Integer.parseInt(bf.readLine());
+        }
+
+        for(int i = 0;i<2;i++){
+            stk = new StringTokenizer(bf.readLine());
+            templen = 0;
+            int start = Integer.parseInt(stk.nextToken());
+            int end = Integer.parseInt(stk.nextToken());
+            for(int j = start;j<=end;j++){
+                arr[j] = 0;
+            }
+            
+            // 떨어뜨리기
+            for(int j = 0;j<arrlen;j++){
+                if(arr[j]!= 0){
+                    temp[templen++] = arr[j];
+                }
+            }
+            // 떨어뜨린 결과 반영하기
+            for(int j = 0;j<templen;j++){
+                arr[j] = temp[j];
+            }
+            // 떨어뜨렸으므로 줄어든 길이만큼 사용하게 만들기
+            arrlen = templen;
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append(arrlen).append("\n");
+        for(int i = arrlen-1;i>-1;i--){
+            sb.append(arr[i]).append("\n");
+        }
+        System.out.print(sb);
+    }
+    
+}
