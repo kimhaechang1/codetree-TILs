fecaa4ce9d7f1a5786156eec6ac1497dc33798a8
kimhaechang <81299056+kimhaechang1@users.noreply.github.com>
2023-12-16 16:00:55 +0900

    Title: 아름다운 수, Time: 89ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 231216/아름다운 수/beautiful-number.java
@@ -0,0 +1,86 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int n;
+    static int cnt;
+    static int [] res;
+    public static void main(String[] args) throws Exception{
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        cnt = 0;
+        res = new int[n];
+        dfs(0);
+        System.out.println(cnt);
+    }
+    static void dfs(int depth){
+        if(depth == n){
+            if(chk(res)){
+                cnt++;
+            }
+            return;
+        }
+
+        for(int i = 1;i<=4;i++){
+            res[depth] = i;
+            dfs(depth+1);
+        }
+    }
+    static boolean chk(int [] arr){
+        int prev = 0;
+        int cnt = 0;
+        for(int i = 0;i<arr.length;i++){
+            if(prev == arr[i]){
+                if(prev == 2){
+                    if(cnt == 2){
+                        cnt = 0;
+                    }else{
+                        cnt++;
+                    }
+                }else if(prev == 3){
+                    if(cnt == 3){
+                        cnt = 0;
+                    }else{
+                        cnt++;
+                    }
+                }else if(prev == 4){
+                    if(cnt == 4){
+                        cnt = 0;
+                    }else{
+                        cnt++;
+                    }
+                }
+            }else{
+                if(prev == 2){
+                    if(cnt != 2){
+                        return false;
+                    }
+                }else if(prev == 3){
+                    if(cnt != 3){
+                        return false;
+                    }
+                }else if(prev == 4){
+                    if(cnt != 4){
+                        return false;
+                    }
+                }
+                prev = arr[i];
+                cnt = 1;
+            }
+        }
+        if(prev == 2){
+            if(cnt != 2){
+                return false;
+            }
+        }else if(prev == 3){
+            if(cnt != 3){
+                return false;
+            }
+        }else if(prev == 4){
+            if(cnt != 4){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+}
