ce464e3a20ccc7f469ae7a2c693035db9e34e033
kimhaechang <81299056+kimhaechang1@users.noreply.github.com>
2023-12-20 13:56:12 +0900

    Title: n개 중에 m개 뽑기, Time: 110ms, Memory: 8MB, Status: Passed - CodeTree

--- 231220/n개 중에 m개 뽑기/n-choose-m.java
+++ 231220/n개 중에 m개 뽑기/n-choose-m.java
@@ -1,53 +1,31 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
+    static StringTokenizer stk;
     static int n;
     static int m;
-    static int [] arr;
-    static int [] sel;
-    static StringTokenizer stk;
+    static StringBuilder sb;
+    static int [] res;
     public static void main(String[] args) throws Exception{
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader bf= new BufferedReader(new InputStreamReader(System.in));
         stk = new StringTokenizer(bf.readLine());
         n = Integer.parseInt(stk.nextToken());
         m = Integer.parseInt(stk.nextToken());
-        arr = new int[n];
-        for(int i = 0;i<n;i++){
-            arr[i] = i+1;
-        }
-        sel = new int[n];
-        Arrays.fill(sel, n-m,n, 1);
-        System.out.println(Arrays.toString(sel));
-        StringBuilder sb= new StringBuilder();
-        do{
-            for(int i = 0;i<n;i++){
-                System.out.println(Arrays.toString(sel));
-                if(sel[i] == 0){
-                    sb.append(arr[i]).append(" ");
-                }
-            }
-            sb.append("\n");
-        }while(np(sel));
+        res = new int[m];
+        sb = new StringBuilder();
+        dfs(1,0);
         System.out.print(sb);
     }
-    static boolean np(int [] prr){
-        int i = prr.length-1;
-        while(i > 0 && prr[i-1] >= prr[i]) i--;
-        if(i == 0) return false;
-        int j = prr.length-1;
-        while(prr[i-1] >= prr[j]) j--;
-        int temp = arr[j];
-        prr[j] = prr[i-1];
-        prr[i-1] = prr[j];
-        int k = arr.length-1;
-        while(i<k){
-            temp= prr[k];
-            prr[k] = prr[i];
-            prr[i] = temp;
-            k--;
-            i++;
+    static void dfs(int cur, int depth){
+        if(depth == m){
+            for(int a : res) sb.append(a).append(" ");
+            sb.append("\n");
+            return;
         }
-        return true;
-    }
+        for(int i = cur;i<=n;i++){
+            res[depth] = i;
+            dfs(i+1, depth+1);
+        }
+    }   
 }
