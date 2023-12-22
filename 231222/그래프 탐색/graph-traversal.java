dc4446ccd489da636edd721494800ec226ef69bc
kimhaechang <81299056+kimhaechang1@users.noreply.github.com>
2023-12-22 11:47:06 +0900

    Title: 그래프 탐색, Time: 92ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 231222/그래프 탐색/graph-traversal.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static ArrayList<Integer> [] g;
+    static boolean [] v;
+    static int n, m;
+    static StringTokenizer stk;
+    static int max;
+    public static void main(String[] args) throws Exception{
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        stk = new StringTokenizer(bf.readLine());
+        n = Integer.parseInt(stk.nextToken());
+        m = Integer.parseInt(stk.nextToken());
+        v = new boolean[n+1];
+        g = new ArrayList[n+1];
+        for(int i = 1;i<n+1;i++){
+            g[i] = new ArrayList<>();
+        }
+        for(int i = 0;i<m;i++){
+            stk = new StringTokenizer(bf.readLine());
+            int v1 = Integer.parseInt(stk.nextToken());
+            int v2 = Integer.parseInt(stk.nextToken());
+            g[v1].add(v2);
+            g[v2].add(v1);
+        }
+        max = 1;
+        v[1] = true;
+        dfs(0, 1, 0);
+        System.out.println(max);
+    }
+    static void dfs(int depth, int pres, int cnt){
+        max = Math.max(cnt, max);
+        for(int vertex : g[pres]){
+            if(v[vertex]) continue;
+            v[vertex] = true;
+            dfs(depth+1, vertex, cnt+1);
+        }
+    }
+}
