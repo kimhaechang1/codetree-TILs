c0b4770d08844055984d316e8a2c0be55e432a51
kimhaechang <81299056+kimhaechang1@users.noreply.github.com>
2023-12-22 11:19:07 +0900

    Title: 외판원 순회, Time: 91ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 231222/외판원 순회/traveling-salesman-problem.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int n;
+    static int [][] map;
+    static boolean [] v;
+    static int min = Integer.MAX_VALUE;
+    static StringTokenizer stk;
+    public static void main(String[] args) throws Exception{
+        BufferedReader bf= new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(bf.readLine());
+        map = new int[n][n];
+        for(int i = 0;i<n;i++){
+            stk =new StringTokenizer(bf.readLine());
+            for(int j = 0;j<n;j++){
+                map[i][j] = Integer.parseInt(stk.nextToken());
+            }
+        }
+        v = new boolean[n];
+        v[0] = true;
+        dfs(0, 0, 0);   
+        System.out.print(min);
+    }
+    static void dfs(int depth, int pres, int sum){
+        if(min < sum) return;
+        if(depth == n-1){
+            sum += map[pres][0];
+            min = Math.min(min, sum);
+            return;
+        }
+
+        for(int i = 1;i<n;i++){
+            if(v[i]) continue;
+            v[i] = true;
+            dfs(depth+1, i, sum+map[pres][i]);
+            v[i] = false;
+        }
+    }
+}
