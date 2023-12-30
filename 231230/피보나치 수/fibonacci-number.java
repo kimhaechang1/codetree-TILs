ac50e8216bb3eef6b151483a2b177c0b3f5354f4
kimhaechang <81299056+kimhaechang1@users.noreply.github.com>
2023-12-30 14:26:23 +0900

    Title: 피보나치 수, Time: 93ms, Memory: 8MB, Status: Passed - CodeTree

--- 231230/피보나치 수/fibonacci-number.java
+++ 231230/피보나치 수/fibonacci-number.java
@@ -7,7 +7,7 @@ public class Main{
 	public static void main(String [] args) throws Exception{
 		BufferedReader bf =new BufferedReader(new InputStreamReader(System.in));
 		n = Integer.parseInt(bf.readLine());
-		fibo = new int[n+1];
+		fibo = new int[n+3];
 		fibo[0] = 0;
 		fibo[1] = 1;
 		fibo[2] = 1;
