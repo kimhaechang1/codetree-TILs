de249ae19c2904b12394d246fd3be08ef4869bc3
kimhaechang <81299056+kimhaechang1@users.noreply.github.com>
2023-12-30 15:28:12 +0900

    Title: 사각형 채우기 3, Time: 99ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 231230/사각형 채우기 3/rectangle-fill-3.java
+++ 231230/사각형 채우기 3/rectangle-fill-3.java
@@ -18,7 +18,7 @@ public class Main{
 		// + dp[i-2]
 		// 
 		for(int i = 3;i<=n;i++) {
-			dp[i] = (dp[i-1] * 3 + dp[i-2] - dp[i-3]) % 1_000_000_007;
+			dp[i] = (dp[i-1] * 3)  % 1_000_000_007  + (dp[i-2]  % 1_000_000_007) - (dp[i-3]  % 1_000_000_007);
 		}
 		System.out.print(dp[n]);
 	}
