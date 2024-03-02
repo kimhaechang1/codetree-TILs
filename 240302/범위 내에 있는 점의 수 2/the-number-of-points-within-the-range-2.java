e0108fcc43df36b0f0c1f8909174d5e440f998fb
kimhaechang <khc9812121@gmail.com>
2024-03-02 21:41:44 +0900

    Title: 범위 내에 있는 점의 수 2, Time: 647ms, Memory: 41MB, Status: Passed - CodeTree

--- 240302/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.java
+++ 240302/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.java
@@ -11,13 +11,13 @@ public class Main {
         n = Integer.parseInt(stk.nextToken());
         q = Integer.parseInt(stk.nextToken());
         stk = new StringTokenizer(bf.readLine());
-        line = new int[1000001];
+        line = new int[1000002];
         for(int i = 0;i<n;i++){
             int point = Integer.parseInt(stk.nextToken());
             line[point] = 1;
         }
-        int [] s = new int[1000000+1];
-        for(int i=1;i<1000000+1;i++){
+        int [] s = new int[1000000+2];
+        for(int i=1;i<1000000+2;i++){
             s[i] = s[i-1] + line[i];
         }
         StringBuilder sb = new StringBuilder();
@@ -25,7 +25,8 @@ public class Main {
             stk = new StringTokenizer(bf.readLine());
             int a = Integer.parseInt(stk.nextToken());
             int b = Integer.parseInt(stk.nextToken());
-            sb.append(s[b] - s[a-1]).append("\n");
+            if(a == 0) sb.append(s[b]).append("\n");
+            else sb.append(s[b] - s[a-1]).append("\n");
         }
         System.out.print(sb);
     }
