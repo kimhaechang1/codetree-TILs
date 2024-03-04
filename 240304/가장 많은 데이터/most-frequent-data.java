cb0935b7e0efe9853ca65705bd76446074b2828b
kimhaechang <khc9812121@gmail.com>
2024-03-04 22:19:59 +0900

    Title: 가장 많은 데이터, Time: 86ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240304/가장 많은 데이터/most-frequent-data.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int n;
+    public static void main(String[] args) throws Exception{
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        HashMap<String, Integer> map = new HashMap<>();
+        n = Integer.parseInt(bf.readLine());
+        int max = 0;
+        while( n-- > 0){
+            String key = bf.readLine();
+            int value = 1;
+            if(map.containsKey(key)){
+                value = map.get(key)+1;
+                max = Math.max(max, value);
+            }
+            map.put(key, value);
+        }
+        System.out.print(max);
+    }
+}
