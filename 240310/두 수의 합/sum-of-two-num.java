edc25446287958b900b1c12a7d60c5daf0f7d6ab
kimhaechang <khc9812121@gmail.com>
2024-03-10 15:46:52 +0900

    Title: 두 수의 합, Time: 417ms, Memory: 26MB, Status: Passed - CodeTree

--- 240310/두 수의 합/sum-of-two-num.java
+++ 240310/두 수의 합/sum-of-two-num.java
@@ -3,7 +3,7 @@ import java.io.*;
 
 public class Main {
     static StringTokenizer stk;
-    static HashMap<Integer, Integer> map;
+    static HashMap<Long, Integer> map;
     static int n;
     static int k;
     public static void main(String[] args) throws Exception{
@@ -13,54 +13,28 @@ public class Main {
         k = Integer.parseInt(stk.nextToken());
         map = new HashMap<>();
         stk = new StringTokenizer(bf.readLine());
-        int [] key = new int[n];
+        long [] key = new long[n];
         for(int i = 0;i<n;i++){
             int a = Integer.parseInt(stk.nextToken());
             key[i] = a;
         }
-        int left = 0;
-        int right = 1;
-        Arrays.sort(key);
         int cnt = 0;
-        //System.out.println(Arrays.toString(key));
-        if(n < 2) {
-        	System.out.println(0);
-        }else {
-        	while(left < n) {
-            	//System.out.println("left : "+left+" right : "+right);
-            	//System.out.println(" lval : "+key[left]+" rval : "+key[right]);
-            	long sum = key[left] + key[right];
-            	if(sum > k) {
-            		left++;
-            		if(left + 1 < n) {
-            			right = left+1;
-            		}else {
-            			break;
-            		}
-            	}else if(sum == k) {
-            		right++;
-            		cnt += 1;
-//            		if(left + 1 < n) {
-//            			right = left+1;
-//            		}else {
-//            			break;
-//            		}
-            	}else {
-            		right++;
-            	}
-            	if(right == n) {
-            		if(sum <= k) {
-        				left++;
-        			}
-            		if(left + 1 < n) {	
-            			right = left+1;
-            		}else {
-            			break;
-            		}
-            	}
-            	
+        // 두 수의 합 이기 때문에, 합을 만들 수 있는 나머지 값을 찾아서 그 경우의수를 만든다.
+        // 또한 나머지 수를 통해 그 다음수를 만들 수 있는 경우 미리 해당 숫자의 경우의 수를 수열에서 만들어서 곱연산이라고 착각할 수 있다.
+        // 어짜피 contstructive 형 문제는 쌓아지면서 답을 도출해내기 때문에, 덧셈연산을 해도 답이 나온다.
+        for(int i= 0;i<n;i++){
+            long diff = k - key[i];
+            if(map.containsKey(diff)){
+                cnt += map.get(diff);
+            }
+
+            if(!map.containsKey(key[i])){
+                map.put(key[i], 1);
+            }else{
+                map.put(key[i], map.get(key[i])+1);
             }
         }
+        
         System.out.print(cnt);
     }
 }
