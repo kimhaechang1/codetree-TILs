ce982a0a971d70a53aca016b4ffc512af26df91c
kimhaechang <khc9812121@gmail.com>
2024-07-18 14:25:14 +0900

    Title: 세 수의 합, Time: 411ms, Memory: 29MB, Status: Passed - CodeTree

--- 240718/세 수의 합/sum-of-three-num.java
+++ 240718/세 수의 합/sum-of-three-num.java
@@ -11,35 +11,25 @@ public class Main {
         n = Integer.parseInt(stk.nextToken());
         k = Integer.parseInt(stk.nextToken());
         
-        HashMap<Integer, Integer> map = new HashMap<>();
         stk = new StringTokenizer(bf.readLine());
         arr = new int[n];
         for(int i= 0;i<n;i++){
             arr[i] = Integer.parseInt(stk.nextToken());
-            if(map.containsKey(arr[i])){
-                map.put(arr[i], map.get(arr[i])+1);
-            }else{
-                map.put(arr[i], 1);
-            }
         }
         int cnt = 0;
-        // 수하나를 고정시켜놓고
-        // 나머지 수 하나에따라 나올수 있는 경우의수를 더한다.
-        // 이때, 중복을 최대한 제거하기 위해서는 끝범위 하나를 고정시켜놓고
-        // 그 사이에 올 수 있는 숫자 하나 뽑았을 때, 나머지 숫자에 대한 경우의 수를 선택한다.
-        Arrays.sort(arr);
         for(int i= 0;i<n-2;i++){
-            long sum = arr[i];
-            int s = i+1;
-            int e = n-1;
-            while(s < e){
-                if(sum + arr[s] + arr[e] < k){
-                    s++;
-                }else if(sum + arr[s] + arr[e] == k){
-                    s++;
-                    cnt++;
-                }else{
-                    e--;
+            // 수 하나 고정시켜놓고
+            // 나머지 수들 중에서 2개를 뽑는 조합을 찾는다.
+            // 이 때 고정수 기준으로 매번 새로운 조합인것을 연산에서 잊으면 안된다.
+            HashMap<Integer, Integer> map = new HashMap<>();
+            int find = k - arr[i];
+            for(int j= i+1;j<n;j++){
+                map.put(arr[j], map.get(arr[j]) == null ? 1 : map.get(arr[j]) + 1);
+            }
+            for(int j = i+1;j<n;j++){
+                map.put(arr[j], map.get(arr[j])-1);
+                if(map.containsKey(find - arr[j])){
+                    cnt += map.get(find - arr[j]);
                 }
             }
         }
