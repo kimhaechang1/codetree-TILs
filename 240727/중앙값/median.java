e3dd95d3483a78bd40d231f117404e416fee0a68
kimhaechang <khc9812121@gmail.com>
2024-07-27 13:38:02 +0900

    Title: 중앙값, Time: 329ms, Memory: 21MB, Status: Wrong Answer - CodeTree

--- 240727/중앙값/median.java
+++ 240727/중앙값/median.java
@@ -22,44 +22,26 @@ public class Main {
             int cnt = 2;
             StringBuilder sb1 = new StringBuilder();
             sb1.append(arr[0]).append(" ");
-            min.add(arr[0]);
+            max.add(arr[0]);
             while(cnt - 1 < m){
-                if(max.size() == 0){
-                    if(min.peek() < arr[cnt-1]){
+                if(max.size() > min.size()){
+                    if(max.peek() <= arr[cnt-1]){
+                        min.add(max.poll());
                         max.add(arr[cnt-1]);
                     }else{
-                        max.add(min.poll());
                         min.add(arr[cnt-1]);
                     }
                 } else {
                     if(max.peek() <= arr[cnt-1]){
-                        if(max.size()+1 == min.size()){
-                            max.add(arr[cnt-1]);
-                        }else{
-                            min.add(max.poll());
-                            max.add(arr[cnt-1]);
-                        }
-                    }else{
-                        if(min.peek() >= arr[cnt-1]){
-                            if(min.size() > max.size()){
-                                max.add(min.poll());
-                                min.add(arr[cnt-1]);
-                            }else if(min.size() == max.size()){
-                                min.add(arr[cnt-1]);
-                            }
-                        } else {
-                            if(min.size() > max.size()){
-                                max.add(min.poll());
-                                min.add(arr[cnt-1]);
-                            }else if(min.size() == max.size()){
-                                min.add(arr[cnt-1]);
-                            }
-                        }
+                        max.add(arr[cnt-1]);
+                    }else {
+                        max.add(min.poll());
+                        min.add(arr[cnt-1]);
                     }
-                    
                 }
+                // System.out.println("minSize: "+min.size()+" maxSize: "+max.size());
                 if(cnt % 2 != 0){
-                    sb1.append(min.peek()).append(" ");
+                    sb1.append(max.peek()).append(" ");
                 }
                 cnt++;
             }
