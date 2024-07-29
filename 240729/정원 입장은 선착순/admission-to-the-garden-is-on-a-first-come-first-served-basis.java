5b86b3d24f195cebedf7bdc54599f8ba0a489bf8
kimhaechang <khc9812121@gmail.com>
2024-07-29 12:35:36 +0900

    Title: 정원 입장은 선착순, Time: 233ms, Memory: 13MB, Status: Wrong Answer - CodeTree

---
+++ 240729/정원 입장은 선착순/admission-to-the-garden-is-on-a-first-come-first-served-basis.java
@@ -0,0 +1,54 @@
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+    static StringTokenizer stk;
+    static int n;
+    static int[][] info;
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(bf.readLine());
+        info = new int[n][3]; // n: 10만
+        for(int i = 0;i<n;i++){
+            stk = new StringTokenizer(bf.readLine());
+            info[i][0] = Integer.parseInt(stk.nextToken()); // a: ~ 10억
+            info[i][1] = Integer.parseInt(stk.nextToken()); // t: ~ 10000
+            info[i][2] = i; // 번호표 ~ 1 ~ N
+        }
+        // 이들 중 가장 오래 기다려야 하는 사람이 기다리는 시간을 출력
+        // 정원에는 한사람만 들어갈 수 있고, 기다리는사람이 여럿이라면 숫자가 작은 사람부터 들어갈 수 있음
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b)->{
+            return a[2] - b[2];
+        }); // 번호표에 따라 오름차순
+
+        Arrays.sort(info, (a, b)-> { 
+            if(a[0] == b[0]){
+                return a[2] - b[2];
+            }
+            return a[0] - b[0];
+        });
+        // 먼저 도착한 순서로 오름차순
+        // 도착순서가 같다면, 번호순으로 오름차순
+        int ans = 0;
+        int pTime = info[0][0] + info[0][1];
+        int ptr = 1;
+        for(;ptr<n;){
+            while(ptr < n && info[ptr][0] < pTime){
+                pq.add(new int[]{info[ptr][0], info[ptr][1], info[ptr][2]});
+                ptr++;
+            }
+            while(!pq.isEmpty()){
+                int[] now = pq.poll();
+                ans = Math.max(ans, (pTime - now[0]));
+                pTime += now[1];
+            }
+
+            if(ptr < n && info[ptr][0] >= pTime){
+                pTime = info[ptr][0] + info[ptr][1];
+            }
+            ptr++;
+        }
+        System.out.println(ans);
+    }
+}
