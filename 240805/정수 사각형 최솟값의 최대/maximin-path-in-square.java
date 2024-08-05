86111db638afc2732adc29685a0310629e595144
kimhaechang <khc9812121@gmail.com>
2024-08-05 13:43:48 +0900

    Title: 정수 사각형 최솟값의 최대, Time: 99ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240805/정수 사각형 최솟값의 최대/maximin-path-in-square.java
+++ 240805/정수 사각형 최솟값의 최대/maximin-path-in-square.java
@@ -7,6 +7,7 @@ public class Main {
     static int[][] map;
     static int[] dy = {0,1};
     static int[] dx = {1,0};
+    static int[][] dp;
     public static void main(String[] args) throws Exception{
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(bf.readLine());
@@ -23,23 +24,35 @@ public class Main {
         // 결국 어떤 지점 i, j로 올 수있는 방법은 현재 문제에서 i-1,j 혹은 i, j-1이 되는데
         // 이 때 각각의 최솟값들 중 최대를 선택하는것이 중요하다. 왜냐하면 그래야 "경로상의 최대" 가 되니까
         // 추가적으로 그럼 map[i][j]는 어떻게 비교 할 것인가? 해당 땅을 밟기 위한 조건만 갖추면된다. 즉, 지금까지의 최소이자 최대보다도 더 작다면 갱신인 셈
-
-        int[][] dp =  new int[n][n];
-        dp[0][0] = map[0][0];
-        for(int i = 1;i<n;i++)
-            dp[0][i] = Math.min(map[0][i], dp[0][i-1]);
-
-        for(int i = 1;i<n;i++)
-            dp[i][0] = Math.min(map[i][0], dp[i-1][0]);
+        dp = new int[n][n];
+        for(int i = 0;i<n;i++){
+            Arrays.fill(dp[i], -1);
+        }
         
-        for(int i = 1;i<n;i++){
-            for(int j= 1;j<n;j++){
-                dp[i][j] = map[i][j];
-                dp[i][j] = Math.min(Math.max(dp[i-1][j], dp[i][j-1]), dp[i][j]);
-            }
+        dfs(0,0);
+
+        System.out.println(dp[0][0]);
+    }
+    static int dfs(int y, int x){
+        if(dp[y][x] != -1){
+            return dp[y][x];
         }
 
-        System.out.println(dp[n-1][n-1]);
+        if(y == n-1 && x == n-1){
+            return map[n-1][n-1];
+        }
+        dp[y][x] = map[y][x];
+        int value = 0;
+        for(int dir = 0;dir<2;dir++){
+            int ny = y + dy[dir];
+            int nx = x + dx[dir];
+            if(OOB(ny, nx)) continue;
+            value = Math.max(value, Math.min(dfs(ny, nx), dp[y][x]));
+        }
 
+        return dp[y][x] = value;
+    }
+    static boolean OOB(int y, int x){
+        return y >= n || y < 0 || x >=n || x < 0;
     }
 }
