416c7a73d41521feda157e49de119f28646467b3
kimhaechang <khc9812121@gmail.com>
2024-08-12 15:04:43 +0900

    Title: 두 수의 합, Time: 470ms, Memory: 26MB, Status: Passed - CodeTree

---
+++ 240812/두 수의 합/sum-of-two-num.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static StringTokenizer stk;
+    static int n;
+    static int k;
+    static int[] arr;
+    public static void main(String[] args) throws Exception{
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        stk = new StringTokenizer(bf.readLine());
+        n = Integer.parseInt(stk.nextToken());
+        k = Integer.parseInt(stk.nextToken());
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        arr = new int[n];
+        stk = new StringTokenizer(bf.readLine());
+        for(int i= 0;i<n;i++){
+            arr[i] = Integer.parseInt(stk.nextToken());
+            map.put(arr[i], map.get(arr[i]) == null ? 1 : map.get(arr[i]) + 1);
+        }
+        int ans = 0;
+        for(int i= 0;i<n;i++){
+            int diff = k - arr[i];
+            // 목표로 하는 숫자로부터 얼마나 값이 모자란지 파악한다.
+
+            if(map.containsKey(diff)){
+                map.put(arr[i], map.get(arr[i]) - 1);
+                ans += map.get(diff);
+            }
+        }
+        System.out.println(ans);
+    }
+}
