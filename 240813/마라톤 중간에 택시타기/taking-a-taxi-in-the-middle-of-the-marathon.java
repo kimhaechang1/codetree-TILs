4a4d6b5f94a19be7ba4feb074e6d123246fff512
kimhaechang <khc9812121@gmail.com>
2024-08-13 09:50:27 +0900

    Title: 마라톤 중간에 택시타기, Time: 371ms, Memory: 23MB, Status: Passed - CodeTree

--- 240813/마라톤 중간에 택시타기/taking-a-taxi-in-the-middle-of-the-marathon.java
+++ 240813/마라톤 중간에 택시타기/taking-a-taxi-in-the-middle-of-the-marathon.java
@@ -11,11 +11,14 @@ public class Main {
         // 그러면 건너뛸 지점을 제외한 인접차의 합을 구할 수 있다.
         // 건너뛸 지점은 어떻게 구하는가? 생각해보면 건너뛸 대상이 되는 지점 이전점과 이후점의 사이거리를 구하면 그만이다.
         // 인접차의 구간합 문제에서 활용
+        // 좌표점을 두개로 나눠서 풀면 안될듯
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(bf.readLine());
-        int[] xrr = new int[n+1];
-        int[] yrr = new int[n+1];
-        for(int i = 1;i<n+1;i++){
+        int[] disL= new int[n];
+        int[] disR= new int[n];
+        int[] xrr = new int[n];
+        int[] yrr = new int[n];
+        for(int i = 0;i<n;i++){
             stk = new StringTokenizer(bf.readLine());
             int y = Integer.parseInt(stk.nextToken());
             int x = Integer.parseInt(stk.nextToken());
@@ -24,27 +27,21 @@ public class Main {
             yrr[i] = y;
         }
 
-        int[] XL = new int[n+1];
-        int[] YL = new int[n+1];
-        int[] XR = new int[n+1];
-        int[] YR = new int[n+1];
-        for(int i = 1;i<n+1;i++){
-            XL[i] = XL[i-1] + (Math.abs(xrr[i-1] - xrr[i]));
-            YL[i] = YL[i-1] + (Math.abs(yrr[i-1] - yrr[i]));
+        
+        for(int i = 1;i<n;i++){
+            disL[i] = disL[i-1] + (Math.abs(xrr[i-1] - xrr[i]) + Math.abs(yrr[i-1] - yrr[i]));
         }
 
-        for(int i = n-1;i > -1;i--){
-            XR[i] = XR[i+1] + (Math.abs(xrr[i+1] - xrr[i]));
-            YR[i] = YR[i+1] + (Math.abs(yrr[i+1] - yrr[i]));
+        for(int i = n-2;i>-1;i--){
+            disR[i] = disR[i+1] + (Math.abs(xrr[i+1] - xrr[i]) + Math.abs(yrr[i+1] - yrr[i]));
         }
 
         int min = Integer.MAX_VALUE;
         
-        for(int i= 2;i<=n-1;i++){
-            int absX = XL[i-1] + XR[i+1] + Math.abs(xrr[i-1] - xrr[i+1]);
-            int absY = YL[i-1] + YR[i+1] + Math.abs(yrr[i-1] - yrr[i+1]);
 
-            min = Math.min(absX + absY, min);
+        for(int i= 1;i<n-1;i++){
+            int absMin = disL[i-1] + disR[i+1] + Math.abs(xrr[i+1] - xrr[i-1]) + Math.abs(yrr[i+1] - yrr[i-1]);
+            min = Math.min(absMin, min);
         }
         System.out.print(min);
     }
