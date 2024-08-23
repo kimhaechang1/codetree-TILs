eea757f1720eeb59edd35db39d8c47b1c6037bb2
kimhaechang <khc9812121@gmail.com>
2024-08-23 16:19:10 +0900

    Title: 가장 많이 겹치는 구간 2, Time: 580ms, Memory: 23MB, Status: Wrong Answer - CodeTree

---
+++ 240823/가장 많이 겹치는 구간 2/section-with-maximum-overlap-2.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static StringTokenizer stk;
+    static int n;
+    public static void main(String[] args) throws Exception{
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(bf.readLine());
+        TreeMap<Integer, Integer> treeMap = new TreeMap<>();
+        for(int i = 0;i<n;i++) {
+            stk = new StringTokenizer(bf.readLine());
+            int x1 = Integer.parseInt(stk.nextToken());
+            int x2 = Integer.parseInt(stk.nextToken());
+
+            treeMap.put(x1, treeMap.get(x1) == null ? 1 : treeMap.get(x1) + 1);
+            treeMap.put(x2, treeMap.get(x2) == null ? -1 : treeMap.get(x2) - 1);
+        }
+
+        TreeMap<Integer, Integer> resultMap = new TreeMap<>();
+        int sum = 0;
+        for(int x: treeMap.keySet()) {
+            sum += treeMap.get(x);
+            resultMap.put(x, sum);
+        }
+
+        int max = -1;
+        for(int x: resultMap.keySet()) {
+            max = Math.max(max, resultMap.get(x));
+        }
+        int cnt = 0;
+        for(int x: resultMap.keySet()) {
+            if(max == resultMap.get(x)) {
+                cnt++;
+            }
+        }
+        System.out.print(cnt);
+    }
+}
