0dae7779763c9d2081f0e10cdc1c4fd2f489d00b
kimhaechang <khc9812121@gmail.com>
2024-09-21 18:11:43 +0900

    Title: Carry 피하기, Time: 105ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 240921/Carry 피하기/escaping-carry.java
+++ 240921/Carry 피하기/escaping-carry.java
@@ -7,78 +7,38 @@ public class Main {
     static int[][] numbers;
     static boolean[] v;
     static int max;
-    static ArrayDeque<Integer> aDeq;
-    static ArrayDeque<Integer> bDeq;
     public static void main(String[] args) throws Exception{
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(bf.readLine());
         nums= new int[n];
         v = new boolean[n];
-        numbers = new int[n][9];
-        aDeq = new ArrayDeque<>();
-        bDeq = new ArrayDeque<>();
         for(int i = 0;i<n;i++) {
-            String n = bf.readLine();
-            nums[i] = Integer.parseInt(n);
-            for(int j = 8 - (n.length() - 1);j < 9;j++) {
-                numbers[i][j] = (int)(n.charAt( j - (8 - (n.length()-1))) - '0');
-            }
+            nums[i] = Integer.parseInt(bf.readLine());
         }
         max = 1;
-        dfs(0, v, 0);
+        dfs(0, 0, 0);
         System.out.println(max);
     }
-    static void dfs(int pSum, boolean[] selected, int cnt) {
-        for(int i = 0;i<n;i++) {
-            if(selected[i]) continue;
-            if(pSum == 0) {
-                selected[i] = true;
-                pSum += nums[i];
+    static void dfs(int depth, int cnt, int sum) { 
+        if(depth == n) {
+            return;
+        }
+
+        for(int i = depth;i<n;i++) {
+            if(can(sum, i)) {
                 max = Math.max(cnt+1, max);
-                dfs(pSum, selected, cnt+1);
-                selected[i] = false;
-                pSum -= nums[i];
-            }
-            if(check(pSum, i)) {
-                selected[i] = true;
-                pSum += nums[i];
-                max = Math.max(cnt+1, max);
-                dfs(pSum, selected, cnt+1);
-                selected[i] = false;
-                pSum -= nums[i];
+                dfs(depth+1, cnt+1, sum+nums[i]);
             }
         }
     }
-    
-    static boolean check(int pSum, int sIdx) {
-        String pStr = String.valueOf(pSum);
-        String sStr = String.valueOf(nums[sIdx]);
-        for(int i = 0;i<pStr.length();i++) {
-            aDeq.addLast(pStr.charAt(i)-'0');
-        }
-        for(int i = 0;i<sStr.length();i++) {
-            bDeq.addLast(sStr.charAt(i)-'0');
-        }
-        while(!aDeq.isEmpty() && !bDeq.isEmpty()) {
-            int a= aDeq.peekLast();
-            int b= bDeq.peekLast();
-            if ( a + b >= 10 ) {
-                if(!aDeq.isEmpty()) {
-                    aDeq.clear();
-                }
-                if(!bDeq.isEmpty()) {
-                    bDeq.clear();
-                }
-                return false;
-            }
-            aDeq.pollLast();
-            bDeq.pollLast();
-        }
-        if(!aDeq.isEmpty()) {
-            aDeq.clear();
-        }
-        if(!bDeq.isEmpty()) {
-            bDeq.clear();
+    static boolean can(int sum, int idx) {
+        int target = nums[idx];
+        while(sum > 0 || target > 0) {
+            int num1 = target % 10;
+            int num2 = sum % 10;
+            if (num1 + num2 >=10) return false;
+            sum /= 10;
+            target /= 10;
         }
         return true;
     }
