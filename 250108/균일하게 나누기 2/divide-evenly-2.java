6cddc418dde4c7c24f8d391a52c7d28a2bf58f99
kimhaechang <khc9812121@gmail.com>
2025-01-08 14:38:23 +0900

    Title: 균일하게 나누기 2, Time: 156ms, Memory: 26MB, Status: Passed - CodeTree

--- 250108/균일하게 나누기 2/divide-evenly-2.java
+++ 250108/균일하게 나누기 2/divide-evenly-2.java
@@ -51,51 +51,28 @@ public class Main {
             isPointedX[x] = true;
             isPointedY[y] = true;
         }
-        /*for(int i = 1; i <= 13; i++) {
-            for(int j = 1; j <= 13; j++) {
-                System.out.print(coordi[i][j]+" ");
-            }
-            System.out.println();
-        }*/
 
         for(int i = 1; i < 1001; i++) {
             for(int j = 1;j < 1001; j++) {
                 S[i][j] = S[i][j - 1] + S[i - 1][j] - S[i - 1][j - 1] + coordi[i][j];
             }
         }
-        /*System.out.println("##################");
-        for(int i = 1; i <= 13; i++) {
-            for(int j = 1; j <= 13; j++) {
-                System.out.print(S[i][j]+" ");
-            }
-            System.out.println();
-        }*/
 
         // 2차원 평면 누적합인데
         // 2중 for문을 돌면서 각 사분면의 누적합을 구하고 그러한 최대이자 최소를 구해야함\
         int ans = Integer.MAX_VALUE;
-        // System.out.println(S[1][1000]);
         for(int i = 1; i < 1001; i++) {
             for(int j = 1; j < 1001; j++) {
                 if (isPointedY[i] || isPointedX[i]) continue;
                 int max = 0;
-                //System.out.println("y: "+i +" x: "+j);
-                //System.out.println("1: "+S[i][j]);
                 max = Math.max(max, S[i][j]);
-                //System.out.println("2: "+(S[i][1000] - S[i][j]));
                 max = Math.max(max, S[i][1000] - S[i][j]);
-                //System.out.println("3: "+(S[1000][j] - S[i][j]));
                 max = Math.max(max, S[1000][j] - S[i][j]);
-                //System.out.println("4: "+(S[1000][1000] - (S[i][1000] + S[1000][j]) + S[i][j]));
                 max = Math.max(max, S[1000][1000] - (S[i][1000] + S[1000][j]) + S[i][j]);
-                //System.out.println("##########################");
                 if (max == 0) continue;
                 ans = Math.min(ans, max);
             }
         }
-
         System.out.print(ans);
-
     }
-
 }
