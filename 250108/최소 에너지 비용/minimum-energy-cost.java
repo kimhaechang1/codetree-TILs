65ffc4c1246558e2f3f340f35282cdbbde8cfae0
kimhaechang <khc9812121@gmail.com>
2025-01-08 13:43:33 +0900

    Title: 최소 에너지 비용, Time: 125ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 250108/최소 에너지 비용/minimum-energy-cost.java
+++ 250108/최소 에너지 비용/minimum-energy-cost.java
@@ -4,8 +4,8 @@ import java.io.*;
 public class Main {
 
     static int n;
-    static long[] arr;
-    static long[] cost;
+    static int[] arr;
+    static int[] cost;
     static BufferedReader bf;
     static StringTokenizer stk;
 
@@ -27,8 +27,8 @@ public class Main {
     void input() throws Exception {
 
         n = Integer.parseInt(bf.readLine());
-        arr = new long[n];
-        cost = new long[n - 1];
+        arr = new int[n];
+        cost = new int[n - 1];
         stk = new StringTokenizer(bf.readLine());
         for(int i = 0; i < n - 1; i++) {
             cost[i] = Integer.parseInt(stk.nextToken());
@@ -49,26 +49,18 @@ public class Main {
         // 그리고 바로 인접한 칸으로만 이동해야함
         // 이럴땐 인접차에 대한 누적합으로 풀 수 있음
 
-        // 거리는 고정값이나, 그 거리를 이동하는데 총 필요한 비용은 다를 수 있음
-        // 그러면 오른쪽에서 부터 볼때, 맨끝칸은 더이상 이동 할 필요가 없음
-        
-        // 위의 예시를 볼때, 결국 최소 비용이 들어야 하니까 n - 2번째 칸에서 1의 에너지를 채우기 위해서는 4의 코스트가 든다.
-        // 그것말고 다른 대체안이 없다.
-        // 그리고 n - 3을 보면 n - 2 ~ n - 1은 4의 비용을 내어 도달하고 n - 3 ~ n - 2를 6의 비용을 낸다고 하면
-        // 총 10의 비용이 발생하는 것 이지만
-        // n - 3에서 애초에 끝까지 가는 비용을 얻는다고 생각하면 총 cost는 4이고 에너지비용은 8이 들기 때문에 더 싸다.
+        // 다시 생각해보자. 연료를 채우는데 필요한 에너지는 해당 스테이션의 가격이라는 점인데
+        // 이전의 최솟값이 해당 스테이션의 값으로 설정해도 큰 무리가 없다.
+        // 이게 무슨말이냐면, 최솟값인 스테이션이 이전에 나온적 있다면 현재 스테이션을 해당 최솟값으로 두고 다음 필요한 양을 계산하는것이 문제가 없다.
+        // 애초에 이전 최솟값인 스테이션에서 총합을 계산하는것과 누적 최솟값은 변화가 없기 때문이다.
+        // 이것이 가능한 이유는 반드시 한칸씩만 이동하기 때문이다.
 
-        long[] minCost = new long[n];
-        minCost[n - 2] = cost[n - 2] * arr[n - 2];
-        
-        long[] ws = new long[n - 1];
-        ws[n - 2] = cost[n - 2];
-        for(int i = n - 3; i > -1 ; i--) ws[i] = ws[i + 1] + cost[i];
-        
-        for(int i = n - 3; i > -1 ; i--) {
-            minCost[i] = Math.min(minCost[i + 1] + (cost[i] * arr[i]), arr[i] * ws[i]);
-        }
-        System.out.print(minCost[0]);
+        int[] minStation = new int[n];
+        minStation[0] = arr[0];
+        for(int i = 1;i < n; i++) minStation[i] = Math.min(minStation[i - 1], arr[i]);
+        long answer = 0;
+        for(int i = 0;i < n - 1; i++) answer += (minStation[i] * cost[i]);
+        System.out.print(answer);
         
     }
 
