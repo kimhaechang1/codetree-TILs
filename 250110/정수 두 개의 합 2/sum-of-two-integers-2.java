20c6b056e22020200f11a416f4774ceed7c9f7c7
kimhaechang <khc9812121@gmail.com>
2025-01-10 15:14:06 +0900

    Title: 정수 두 개의 합 2, Time: 244ms, Memory: 15MB, Status: Passed - CodeTree

--- 250110/정수 두 개의 합 2/sum-of-two-integers-2.java
+++ 250110/정수 두 개의 합 2/sum-of-two-integers-2.java
@@ -40,25 +40,18 @@ public class Main {
 
     void solve() {
         // 두 원소를 골라 그 합이 k 이하
-        // s + e의 합인건데, k이하라는 조건이 깨지기 전까지 e를 늘려놓고, 
-        // 이게 무슨말인가? e를 고정시켜놓고 s를 늘려도 어짜피 k이하일 수 밖에 없다.
-        // 굳이 고정된 e안에서 모든 s의 경우를 확인 할 필요가 없음
-
-        // 그런다음 s를 늘렸을 때, 만약 s + e의 합이 여전히 k이하라면, e - s + 1 만큼 또 더하면됨
-        // 그렇게 하다가, 깨지는 순간이 오면 e를 하나 줄이면됨
+        // 두 포인터에 대해서 양쪽 끝에서 시작하는가, 아니면 양쪽이 같은지점에서 출발하는가 둘 중 하나다.
         Arrays.sort(arr);
         int start = 0;
         int cnt = 0;
-        int end = 1;
+        int end = arr.length - 1;
         while(start < end) {
-            while(end < n && arr[start] + arr[end] <= s) {
-                end++;
-            }
-            if (end == n || arr[start] + arr[end] > s) end--;
             if (arr[start] + arr[end] <= s) {
                 cnt += (end - start);
+                start++;
+            } else {
+                end--;
             }
-            start++;
         }
         System.out.print(cnt);
     }
